290368990d94987f19f88a49d2f511b36d809445
Jitu <jitendrajat660@gmail.com>
2022-05-13 23:07:28 +0530

    Added functionalities to change the color to black or random by buttons.

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 const container = document.querySelector(".container");
-
+let colorType = "black";
+// Funtions to create, erase and fill the grid
 function createGrid(size) {
   const cellSize = 650 / size;
   for (let i = 0; i < size; i++) {
@@ -16,7 +17,48 @@ function createGrid(size) {
     container.appendChild(row);
   }
 }
+function removeGrid() {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+}
+function fillColor() {
+  if (colorType === "Black") {
+    this.style.backgroundColor = "black";
+  } else if (colorType === "Random") {
+    this.style.backgroundColor = generateRandomColor();
+  }
+}
+function generateRandomColor() {
+  // Function to generate a random hex color value so that it can be used to function the random button
+  let choices = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += choices[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 
+// Reset button functionality
+const resetButton = document.querySelector("#reset");
+resetButton.addEventListener("click", () => {
+  removeGrid();
+  createGrid(16);
+});
+
+// Black button functionality
+const blackButton = document.querySelector("#black");
+blackButton.addEventListener("click", () => {
+  colorType = "Black";
+});
+
+// Random button functionality
+
+const randomButton = document.querySelector("#random");
+randomButton.addEventListener("click", () => {
+  colorType = "Random";
+});
+// slider to adjust the grid size
 const slider = document.querySelector("#slider>input");
 const sliderVal = document.querySelector("#slider>p");
 slider.addEventListener("input", () => {
@@ -26,13 +68,4 @@ slider.addEventListener("input", () => {
   createGrid(newSize);
 });
 
-function fillColor() {
-  this.style.backgroundColor = "black";
-}
-
-function removeGrid() {
-  while (container.firstChild) {
-    container.removeChild(container.firstChild);
-  }
-}
 createGrid(16);
